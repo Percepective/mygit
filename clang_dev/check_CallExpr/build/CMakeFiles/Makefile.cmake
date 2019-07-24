87bd540e378683f4e3b525482646e90b5493b0f1
Percepective <sxy1140110427@163.com>
2019-07-24 14:45:19 +0800

    version2.0

--- clang_dev/check_CallExpr/build/CMakeFiles/Makefile.cmake
+++ clang_dev/check_CallExpr/build/CMakeFiles/Makefile.cmake
@@ -11,7 +11,6 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.10.2/CMakeCCompiler.cmake"
   "CMakeFiles/3.10.2/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.10.2/CMakeSystem.cmake"
-  "../cmake-modules/FindLibClang.cmake"
   "/usr/share/cmake-3.10/Modules/CMakeCInformation.cmake"
   "/usr/share/cmake-3.10/Modules/CMakeCXXInformation.cmake"
   "/usr/share/cmake-3.10/Modules/CMakeCommonLanguageInclude.cmake"
